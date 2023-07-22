9b301c81f9c269d82442843f08bd61785fe27b18
Artem Vlasenko <arvlas78@gmail.com>
2023-07-22 15:43:39 +0300

    new command /random_image

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import os
 
 import logging
 import asyncio
-
+from aiogram.exceptions import TelegramBadRequest
 from aiogram import Bot, Dispatcher, Router, types
 from aiogram.filters import Command, CommandObject
-from aiogram.types import Message
+from aiogram.types import Message, URLInputFile
 from aiogram import html
 
 from aiogram import F
@@ -55,23 +55,26 @@ async def cmd_start(message: types.Message):
     await message.answer("Як подавати котлети?", reply_markup=keyboard)
 
 
-@router.message(F.text)
-async def text_handler(message: Message) -> None:
-    if message.text == "З пюрешкою":
-        await message.answer(f"{message.from_user.full_name}, з пюрешкою завжди смачніше:)")
-    elif message.text == "Без пюрешки":
-        await message.answer(f"{message.from_user.full_name}, без пюрешки не ситно:(")
-
-
-@router.message(F.text)
-async def text_handler(message: Message) -> None:
+@router.message(Command("random_image"))
+async def get_random_image(message: Message, command: CommandObject) -> None:
     """
-    This handler receive messages with `/start` command
+    /random_image 500 600
+    /random_image 500 -> Required 2 params but 1 given
+    /random_image 500 500 500-> Required 2 params but 3 given
+    /random_image -> Required 2 params but 0 given
+    :param command:
+    :param message:
+    :return:
     """
+    size = command.args.split()
+    if len(size) != 2:
+        await message.answer(f'Required 2 params but {len(size)} given')
+
+    image_from_url = URLInputFile(f"https://random.imagecdn.app/{size[0]}/{size[1]}")
     try:
-        await message.reply(f'Your text is {len(message.text)} characters long!', parse_mode='HTML')
-    except TypeError:
-        await message.reply('Not a text!')
+        await message.answer_photo(image_from_url)
+    except TelegramBadRequest:
+        await message.answer('Photo invalid dimensions')
 
 
 async def main() -> None:
